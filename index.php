db027aa80a50d67b95682a48c87c20f5f5facca5
nords_codes <nordeley_corradini@estudante.sesisenai.org.br>
2026-08-17 08:38:29 -0300

    feat - adicionei os codigos pro index

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastros</title>
+</head>
+<body>
+    <form id="form_cadastro"><h2>Cadastrar Usuario</h2>
+
+    <Label for="nome">Nome</Label>
+    <input type="text" id="nome" placeholder="insira seu nome">
+
+    </form>
+</body>
+</html>
