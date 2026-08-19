8c72300e86f7c64b987ef8d1e2610a1a1f297d35
nords_codes <nordeley_corradini@estudante.sesisenai.org.br>
2026-08-19 12:57:01 -0300

    up - organizei ocodigo

--- public/cadastrar_usuario.php
+++ public/cadastrar_usuario.php
@@ -13,3 +13,26 @@ if ($stmt->execute()){
 } else{
     echo "Erro ao cadastrar usuario";
 }
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="cadastrar_pratos.php" method="POST">
+        <label class="informacoes">Nome do prato</label>
+        <input type="text" id="nome_prato" placeholder ="insira o nome do prato..." required>
+
+        <label class="informacoes">descrição do prato</label>
+        <input type="text" id="descricao" placeholder="insira a descrição do prato...">
+
+        <label class="informacoes">categoria</label>
+        <input type="text" id="categoria" placeholder="insira a categoria do prato..." required>
+
+        <label class="informacoes">Preço</label>
+        <input type="number" id="preco" placeholder="insira preço ex:(0.00)" required step="0.01" min="0">
+    </form>
+</body>
+</html>
