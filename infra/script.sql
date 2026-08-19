b832ad283f4664aa8736fd324be570b90c19bd9f
nords_codes <nordeley_corradini@estudante.sesisenai.org.br>
2026-08-19 08:09:26 -0300

    feat - adicionei a tabela pratos no banco de dados

--- infra/script.sql
+++ infra/script.sql
@@ -5,5 +5,12 @@ USE restaurante_nordsleite
 CREATE TABLE usuario (
     id int auto_increment primary key,
     nome varchar(150) NOT NULL,
-    email varchar(200),
+    email varchar(200) NOT NULL,
+)
+CREATE TABLE pratos(
+    id int auto_increment primary key,
+    nome_prato varchar(100) NOT NULL,
+    preco decimal (10, 2) NOT NULL,
+    categoria varchar(100) NOT NULL,
+    descricao varchar(200),
 )
